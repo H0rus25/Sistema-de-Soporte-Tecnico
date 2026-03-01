48123ac843c1f45985f0b02fcf9ebd2db0602fbd
Juan <jdrodrigue987@gmail.com>
2026-03-01 18:27:31 -0400

    Actualizacion del gitignore para ocultar las fotos

--- DB/init.sql
+++
@@ -1,9 +0,0 @@
-USE soporte_tecnico;
-
-CREATE TABLE tickets (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario VARCHAR(100) NOT NULL,
-    descripcion_falla TEXT NOT NULL,
-    fecha_reporte DATETIME DEFAULT CURRENT_TIMESTAMP,
-    resuelto BOOLEAN DEFAULT FALSE
-);
